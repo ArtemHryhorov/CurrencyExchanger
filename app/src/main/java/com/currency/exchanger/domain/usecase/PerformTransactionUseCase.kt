0876adff3eb51e2841bd9124fd11572af2530100
ArtemHryhorovGeniusee <a.hryhorov@geniusee.com>
2025-01-12 19:03:00 +0200

    Apply changes to user balance after performing transaction

---
+++ app/src/main/java/com/currency/exchanger/domain/usecase/PerformTransactionUseCase.kt
@@ -0,0 +1,97 @@
+package com.currency.exchanger.domain.usecase
+
+import android.util.Log
+import com.currency.exchanger.domain.model.Currency
+import com.currency.exchanger.domain.model.CurrencyBalance
+import com.currency.exchanger.domain.model.UserBalance
+import javax.inject.Inject
+
+class PerformTransactionUseCase @Inject constructor() {
+
+    /**
+     * This should be Back-End call, but we just changing user balance in local DB.
+     */
+    operator fun invoke(
+        userBalance: UserBalance,
+        sellCurrency: Currency,
+        sellAmount: Double,
+        receiveCurrency: Currency,
+        receiveAmount: Double,
+        fee: Double,
+    ): UserBalance {
+        val updatedSellCurrencyBalance = transferCurrencyFromWallet(
+            userBalance = userBalance,
+            sellCurrency = sellCurrency,
+            sellAmount = sellAmount,
+        )
+        val updatedReceiveCurrencyBalance = transferCurrencyToWallet(
+            userBalance = userBalance,
+            receiveCurrency = receiveCurrency,
+            receiveAmount = receiveAmount,
+        )
+
+        return updateUserBalance(
+            userBalance = userBalance,
+            updatedSellCurrencyBalance = updatedSellCurrencyBalance,
+            updatedReceiveCurrencyBalance = updatedReceiveCurrencyBalance,
+            fee = fee,
+        )
+    }
+
+    private fun transferCurrencyFromWallet(
+        userBalance: UserBalance,
+        sellCurrency: Currency,
+        sellAmount: Double,
+    ): CurrencyBalance {
+        val sellCurrencyBalance = userBalance
+            .currencyBalanceList
+            .find { it.currency == sellCurrency }
+            ?: throw Exception("No currency found")
+        Log.d("Convertor", "- $sellAmount ${sellCurrency.name}")
+        return sellCurrencyBalance.copy(amount = sellCurrencyBalance.amount - sellAmount)
+    }
+
+    private fun transferCurrencyToWallet(
+        userBalance: UserBalance,
+        receiveCurrency: Currency,
+        receiveAmount: Double,
+    ): CurrencyBalance {
+        val receiveCurrencyBalance = userBalance
+            .currencyBalanceList
+            .find { it.currency == receiveCurrency }
+        Log.d("Convertor", "+ $receiveAmount ${receiveCurrency.name}")
+        return CurrencyBalance(
+            currency = receiveCurrency,
+            amount = (receiveCurrencyBalance?.amount ?: 0.0) + receiveAmount,
+        )
+    }
+
+    private fun updateUserBalance(
+        userBalance: UserBalance,
+        updatedSellCurrencyBalance: CurrencyBalance,
+        updatedReceiveCurrencyBalance: CurrencyBalance,
+        fee: Double,
+    ): UserBalance {
+        // Update user balance
+        val updatedBalanceList = userBalance
+            .currencyBalanceList
+            .toMutableList()
+        updatedBalanceList.replaceAll {
+            when {
+                (it.currency == updatedSellCurrencyBalance.currency) -> updatedSellCurrencyBalance
+                (it.currency == updatedReceiveCurrencyBalance.currency) -> updatedReceiveCurrencyBalance
+                else -> it
+            }
+        }
+
+        // If receiveCurrency is new to wallet - add this currency to wallet
+        if (updatedBalanceList.any { it.currency == updatedReceiveCurrencyBalance.currency }.not()) {
+            updatedBalanceList.add(updatedReceiveCurrencyBalance)
+        }
+
+        // Transfer fee somewhere if needed
+        Log.d("Convertor", "$fee ${updatedSellCurrencyBalance.currency.name} fee applied")
+
+        return UserBalance(updatedBalanceList)
+    }
+}
